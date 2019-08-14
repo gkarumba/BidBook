ef1ab5d995cf8e96043e5647b76c9a7a13c850fc
gkarumba <gachegua@gmail.com>
2019-08-14 14:53:12 +0300

    Register auction models in the admin.py

--- auction/auctions/admin.py
+++ auction/auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Auction)
+admin.site.register(Bid)
+admin.site.register(Watchlist)
+admin.site.register(Chat)
